e643f169fadc92c07347111a9eab43826395ac5a
ty-hal <tyhalepn16@gmail.com>
2023-01-15 16:16:30 -0500

    test commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/team/swe-project
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/joho/godotenv v1.4.0
+	go.mongodb.org/mongo-driver v1.11.1
+)
---
+++ server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/team/swe-project/router"
+)
+
+func main() {
+	// r := router.Router()
+	fmt.Println("starting the server on port 9000...")
+
+	// log.Fatal(http.ListenAndServe(":9000", r))
+}

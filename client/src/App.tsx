600182c86339186aadd2d1449ee447771abc8ca2
ty-hal <tyhalepn16@gmail.com>
2023-01-17 13:59:00 -0500

    h

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,69 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+          >
             <span className="fs-4">Something</span>
           </a>
 
           <ul className="nav col-2 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
-            <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
+            <li>
+              <a href="#" className="nav-link px-2 link-secondary">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-dark">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-dark">
+                About
+              </a>
+            </li>
           </ul>
 
           <div className="col-md-3 text-end">
-            <button type="button" className="btn btn-outline-primary me-2">Login</button>
-            <button type="button" className="btn btn-primary">Sign-up</button>
+            <button type="button" className="btn btn-outline-primary me-2">
+              Login
+            </button>
+            <button type="button" className="btn btn-primary">
+              Sign-up
+            </button>
           </div>
-
         </header>
-
-
 
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <p className="col-md-4 mb-0 text-muted">&#169; 2023 Company, Inc</p>
 
-          <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-          </a>
+          <a
+            href="/"
+            className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+          ></a>
 
           <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-muted">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-muted">
+                About
+              </a>
+            </li>
           </ul>
         </footer>
       </div>

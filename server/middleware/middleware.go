8806073010e1ce6a0bb028635c2ea6800d15dcdc
Dev Patel <devpatel2556@gmail.com>
2023-01-25 15:42:05 -0500

    create env file

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,7 +15,21 @@ var DB *gorm.DB
 
 func Init() {
 	// Setup connection to PostgreSQL database
-	dsn := "host=db.nyevjnzxzjjckgdworch.supabase.co user=postgres password=Gatorchat@12345 dbname=postgres port=5432"
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get Database Configuration
+	host := os.Getenv("host")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbname := os.Getenv("dbname")
+	port := os.Getenv("port")
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", host, user, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
--- server/router/router.go
+++ server/router/router.go
@@ -1,12 +1,17 @@
 package router
 
 import (
+	"fmt"
+
 	"github.com/gorilla/mux"
 	"github.com/team/swe-project/handlers"
+	"github.com/team/swe-project/middleware"
+	"github.com/team/swe-project/models"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
+	middleware.Init()
 
 	// User Routes
 	router.HandleFunc("/api/users", handlers.GetAllUsers).Methods("GET")
@@ -15,5 +20,8 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/users/{id}", handlers.DeleteUser).Methods("DELETE")
 	router.HandleFunc("/api/user/{id}", handlers.UpdateUser).Methods("PUT")
 
+	// Test getting user
+	user := models.GetUser(1)
+	fmt.Printf("name:%v    | email:%v \n", user.FirstName, user.Email)
 	return router
 }

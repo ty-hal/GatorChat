301b9eec595cc718d73be7fa0bc2f99aff8ffd81
ty-hal <tyhalepn16@gmail.com>
2023-02-04 22:39:19 -0500

    Confirm password must equal password to submit registration form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -49,34 +49,32 @@ const Register = () => {
     if (elem_confirmPassword !== null) {
       if (confirmPassword === "") {
         elem_confirmPassword.classList.add("dark:text-white");
-        elem_confirmPassword.classList.add("dark:focus:ring-blue-500");
-        elem_confirmPassword.classList.add("dark:focus:border-blue-500");
+        elem_confirmPassword.classList.add("focus:ring-blue-500");
+        elem_confirmPassword.classList.add("focus:border-blue-500");
         elem_confirmPassword.classList.add("focus:ring-blue-600");
         elem_confirmPassword.classList.add("focus:border-blue-600");
-
-        elem_confirmPassword.classList.remove("dark:text-red-500");
-        elem_confirmPassword.classList.remove("dark:focus:border-red-600");
-        elem_confirmPassword.classList.remove("dark:focus:border-red-600");
+        elem_confirmPassword.classList.remove("text-red-500");
+        elem_confirmPassword.classList.remove("focus:border-red-600");
       } else if (password !== confirmPassword) {
         elem_confirmPassword.classList.remove("dark:text-white");
-        elem_confirmPassword.classList.remove("dark:focus:ring-blue-500");
-        elem_confirmPassword.classList.remove("dark:focus:border-blue-500");
-        elem_confirmPassword.classList.remove("focus:ring-blue-600");
-        elem_confirmPassword.classList.remove("focus:border-blue-600");
+        elem_confirmPassword.classList.remove("focus:ring-blue-500");
+        elem_confirmPassword.classList.remove("focus:border-blue-500");
+        elem_confirmPassword.classList.remove("dark:focus:ring-blue-600");
+        elem_confirmPassword.classList.remove("dark:focus:border-blue-600");
 
-        elem_confirmPassword.classList.add("dark:text-red-500");
-        elem_confirmPassword.classList.add("dark:focus:border-red-600");
-        elem_confirmPassword.classList.add("dark:focus:border-red-600");
+        elem_confirmPassword.classList.add("text-red-500");
+        elem_confirmPassword.classList.add("focus:border-red-600");
+        elem_confirmPassword.classList.add("focus:border-red-600");
       } else if (password === confirmPassword) {
         elem_confirmPassword.classList.add("dark:text-white");
-        elem_confirmPassword.classList.add("dark:focus:ring-blue-500");
-        elem_confirmPassword.classList.add("dark:focus:border-blue-500");
+        elem_confirmPassword.classList.add("focus:ring-blue-500");
+        elem_confirmPassword.classList.add("focus:border-blue-500");
         elem_confirmPassword.classList.add("focus:ring-blue-600");
         elem_confirmPassword.classList.add("focus:border-blue-600");
 
-        elem_confirmPassword.classList.remove("dark:text-red-500");
-        elem_confirmPassword.classList.remove("dark:focus:border-red-600");
-        elem_confirmPassword.classList.remove("dark:focus:border-red-600");
+        elem_confirmPassword.classList.remove("text-red-500");
+        elem_confirmPassword.classList.remove("focus:border-red-600");
+        elem_confirmPassword.classList.remove("focus:border-red-600");
       }
     }
   }, [confirmPassword]);
@@ -557,7 +555,24 @@ const Register = () => {
               {/* Submit Button  */}
               <button
                 type="submit"
-                className="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                disabled={
+                  first_name &&
+                  last_name &&
+                  email &&
+                  major &&
+                  password === confirmPassword
+                    ? false
+                    : true
+                }
+                className={
+                  first_name &&
+                  last_name &&
+                  email &&
+                  major &&
+                  password === confirmPassword
+                    ? "w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    : "w-full cursor-auto rounded-lg bg-gray-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600  dark:focus:ring-blue-800"
+                }
               >
                 Create an account
               </button>

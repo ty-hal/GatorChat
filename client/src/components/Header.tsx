e3e8bab97994946f29257d45a8eeb08262784645
ty-hal <tyhalepn16@gmail.com>
2023-02-04 20:45:46 -0500

    Dark mode toggle in settings, edited messages and FAQ, added scroll to top on page change

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,6 +11,45 @@ const Layout = () => {
     setShowDropDown(!showDropDown);
   };
 
+  let darkModeText = (
+    <div className="flex w-full items-center ">
+      <span className="mr-2 w-2/3">Dark Mode</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+        />
+      </svg>
+    </div>
+  );
+
+  let lightModeText = (
+    <div className="flex w-full items-center ">
+      <span className="mr-2 w-2/3">Light Mode</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+        />
+      </svg>
+    </div>
+  );
   return (
     <div className="flex flex-wrap border-b-4 border-b-blue-600">
       <section className="relative mx-auto">
@@ -88,28 +127,14 @@ const Layout = () => {
                       </div>
                       <div className="py-1" role="none">
                         <span
-                          className="block px-4 py-2 text-sm text-gray-700 hover:font-semibold"
+                          className="block py-2 pl-4 text-sm text-gray-700 hover:font-semibold"
                           role="menuitem"
                           id="menu-item-6"
                           onClick={() => setDarkMode(!darkMode)}
                         >
-                          Dark Mode
+                          {darkMode ? lightModeText : darkModeText}
                         </span>
-                        {/* Here  */}
-                        <div className=" bg-red-200">
-                          <span className="ml-4 text-sm font-medium text-gray-700 hover:font-semibold">
-                            Toggle me
-                          </span>
-                          <label className="relative inline-flex cursor-pointer items-center">
-                            <input
-                              type="checkbox"
-                              value=""
-                              className="peer sr-only"
-                            />
 
-                            <div className="peer h-5 w-9 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-                          </label>
-                        </div>
                         <span
                           className="block px-4 py-2 text-sm text-gray-700 hover:font-semibold"
                           role="menuitem"

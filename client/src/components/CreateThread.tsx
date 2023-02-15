bc3280812d8ffa7bf29fb3fcb08aacd96338ebcb
ty-hal <tyhalepn16@gmail.com>
2023-02-15 17:40:34 -0500

    Edited the RTE for creating a thread

--- client/src/components/CreateThread.tsx
+++ client/src/components/CreateThread.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+// Rich Text Editor via Tip Tap
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { RTE } from "./RTE";
 
 const CreateThread = () => {
-  const [openEditor, toggleOpenEditor] = useState(false);
-
+  const [openEditor, toggleOpenEditor] = useState(true);
   return (
     <div
-      className="mx-auto w-11/12 cursor-pointer rounded-2xl border-2 border-gray-600 bg-gray-200 py-2 text-center text-lg font-semibold text-gray-900 shadow-xl hover:border-blue-600 dark:bg-gray-800 dark:text-white lg:w-4/5"
+      className="mx-auto w-11/12 cursor-pointer rounded-2xl border-2 border-gray-600 bg-gray-200 py-2  shadow-xl hover:border-blue-600 dark:bg-gray-800 lg:w-4/5"
       id="container"
-      onClick={() => toggleOpenEditor(!openEditor)}
+      // onClick={() => toggleOpenEditor(!openEditor)}
     >
-      <div>Create a thread</div>
-      {openEditor && <div>HI</div>}
+      <div className="text-center text-lg font-semibold text-gray-900 dark:text-white">
+        Create a thread
+      </div>
+      {openEditor && (
+        <div className="text-gray-900 dark:text-white">
+          <div className="text-center font-semibold">Title</div>
+          <div className="text-center font-semibold">Text</div>
+          <RTE />
+        </div>
+      )}
     </div>
   );
 };

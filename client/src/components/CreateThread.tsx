5519f1b259417ba41034bd16ca748acd36611f14
ty-hal <tyhalepn16@gmail.com>
2023-02-15 22:18:35 -0500

    Edited RTE for creating a thread

--- client/src/components/CreateThread.tsx
+++ client/src/components/CreateThread.tsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
-// Rich Text Editor via Tip Tap
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
 import { RTE } from "./RTE";
 
 const CreateThread = () => {
-  const [openEditor, toggleOpenEditor] = useState(true);
+  const [openEditor, toggleOpenEditor] = useState(false);
   return (
     <div
-      className="mx-auto w-11/12 cursor-pointer rounded-2xl border-2 border-gray-600 bg-gray-200 py-2  shadow-xl hover:border-blue-600 dark:bg-gray-800 lg:w-4/5"
+      className="mx-auto w-11/12 cursor-pointer rounded-2xl border-2 border-gray-600 bg-gray-200 py-2 shadow-xl dark:bg-gray-800 lg:w-4/5"
       id="container"
-      // onClick={() => toggleOpenEditor(!openEditor)}
+      onClick={(e) => {
+        e.stopPropagation();
+        toggleOpenEditor(!openEditor);
+      }}
     >
       <div className="text-center text-lg font-semibold text-gray-900 dark:text-white">
         Create a thread
       </div>
       {openEditor && (
-        <div className="text-gray-900 dark:text-white">
-          <div className="text-center font-semibold">Title</div>
-          <div className="text-center font-semibold">Text</div>
+        <div
+          className="cursor-default text-gray-900 dark:text-white"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className="mx-auto w-11/12">
+            <div className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+              Title
+            </div>
+            <input
+              type="text"
+              className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Enter a title..."
+            />
+          </div>
           <RTE />
         </div>
       )}
--- client/src/components/RTE.tsx
+++ client/src/components/RTE.tsx
@@ -1,6 +1,8 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import "../RTEstyling.css";
+import Placeholder from "@tiptap/extension-placeholder";
+import TextAlign from "@tiptap/extension-text-align";
+import CharacterCount from "@tiptap/extension-character-count";
 
 const MenuBar = ({ editor }: any) => {
   if (!editor) {
@@ -8,14 +10,31 @@ const MenuBar = ({ editor }: any) => {
   }
 
   return (
-    <div className="space-x-4 p-2 ">
+    <div className="mx-auto mt-4 w-11/12 space-x-4 bg-gray-600 p-2">
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
-        // className={editor.isActive("bold") ? "is-active" : ""}
-        className=""
+        className={editor.isActive("bold") ? "is-active" : ""}
       >
-        bold
+        <svg
+          fill="#000000"
+          viewBox="0 0 1920 1920"
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 bg-white"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M480.286 822.857h548.571c151.269 0 274.286-123.017 274.286-274.286 0-151.268-123.017-274.285-274.286-274.285H480.286v548.571Zm0 822.857H1166c151.269 0 274.286-123.017 274.286-274.285 0-151.269-123.017-274.286-274.286-274.286H480.286v548.571ZM1166 1920H206V0h822.857c302.537 0 548.572 246.034 548.572 548.571 0 134.263-48.549 257.418-128.778 352.732 159.223 96.137 265.92 270.994 265.92 470.126 0 302.537-246.034 548.571-548.571 548.571Z"
+              fillRule="evenodd"
+            ></path>
+          </g>
+        </svg>
       </button>
       <button
         onClick={() => editor.chain().focus().toggleItalic().run()}
@@ -68,7 +87,30 @@ const MenuBar = ({ editor }: any) => {
       >
         h3
       </button>
-
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
+      >
+        left
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        className={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}
+      >
+        center
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
+      >
+        right
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+        className={editor.isActive({ textAlign: "justify" }) ? "is-active" : ""}
+      >
+        justify
+      </button>
       <button
         onClick={() => editor.chain().focus().toggleBulletList().run()}
         className={editor.isActive("bulletList") ? "is-active" : ""}
@@ -103,18 +145,42 @@ const MenuBar = ({ editor }: any) => {
 
 export const RTE = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Enter text...",
+        emptyNodeClass:
+          "first:before:text-gray-400 first:before:float-left first:before:content-[attr(data-placeholder)] first:before:pointer-events-none first:before:h-0",
+      }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      CharacterCount.configure({
+        limit: 400,
+      }),
+    ],
     content: `
-      <p>
-        Text
-      </p>
+
     `,
   });
 
   return (
-    <div id="RTE-text">
+    <div
+      id="RTE-text"
+      onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
+        e.stopPropagation()
+      }
+    >
       <MenuBar editor={editor} />
-      <EditorContent editor={editor} />
+      <EditorContent
+        editor={editor}
+        id="test"
+        className="mx-auto w-11/12 cursor-text rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        onKeyDown={(e) => console.log(e)}
+      />
+      <div className="mx-auto mt-2 w-11/12 text-right text-gray-400">
+        {editor?.storage?.characterCount.characters()}/400 characters
+      </div>
     </div>
   );
 };

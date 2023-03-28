c9a8c5d000d78d3641b57c009bf134857a7d18cc
ty-hal <tyhalepn16@gmail.com>
2023-03-28 12:06:48 -0400

    Added tests to Thread.cy.ts

--- client/cypress/e2e/Thread.cy.ts
+++ client/cypress/e2e/Thread.cy.ts
@@ -1,18 +1,86 @@
+import "../support/commands.ts"
+
 describe('thread spec', () => 
 {
-  it('create a thread', ()=> {
-    cy.visit("/sign-in");
-    cy.get("#email").type("millersteven@ufl.edu");
-    cy.get("#password").type("KfkGt2J2sAwA9tg");
+  it('create thread', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Create a thread").click();
+    cy.get("#title").type("Cypress Test");
+    cy.get("#text").type("Cypress Text is Here");
     cy.get("#submit").click();
-    cy.get(".mx-auto.w-full > :nth-child(1) > a > .cursor-pointer").click();
-    cy.wait(500);
-    cy.get("#create-a-thread").click();
-    cy.get("#create-a-thread").click();
-    cy.get("#title").type("thread test");
-    //cy.get("#editor-content").type("the grey fox jumped over the candlestick");
-    //cy.get("#submit").click();
+    cy.contains("Cypress Test").should('exist');
+  })
 
-   
+
+  it('edit thread', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Cypress Test").click();
+    cy.wait(3000);
+    cy.get("#thread-menu").click();
+    cy.get("#edit").click();
+    cy.get("#title-edit").click().type(" Edited");
+    cy.contains("Edit thread").click();
+    cy.contains("Cypress Test Edited").should('exist');
+  })
+
+  it('like thread', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Cypress Test Edited").click();
+    cy.wait(3000);
+    cy.get("#num-likes").contains("0");
+    cy.get("#like-button").click();
+    cy.get("#num-likes").contains("1");
+    cy.contains("Cypress Test Edited").should('exist');
+  })
+
+  it('reply to thread', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Cypress Test Edited").click();
+    cy.wait(3000);
+    cy.get("#message-placeholder").click();
+    cy.get("#text").type("Cypress message");
+    cy.get("#submit-message").click();
+    cy.wait(3000);
+    cy.contains("Cypress message").should('exist');
+  })
+
+  it('delete reply', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Cypress Test Edited").click();
+    cy.wait(3000);
+    cy.get("#message-menu").click();
+    cy.get("#delete").click();
+    cy.get("#delete-btn").click();
+    cy.contains("Cypress message").should('not.exist');
+  })
+
+
+  it('delete thread', ()=> {
+    cy.login("random@ufl.edu", "Mypassword@123");
+    cy.visit("/");
+    cy.contains("General").click();
+    cy.wait(3000);
+    cy.contains("Cypress Test Edited").should('exist');
+    cy.contains("Cypress Test Edited").click();
+    cy.wait(3000);
+    cy.get("#thread-menu").click();
+    cy.get("#delete").click();
+    cy.get("#delete-btn").click();
+    cy.contains("Cypress Test Edited").should('not.exist');
   })
 })

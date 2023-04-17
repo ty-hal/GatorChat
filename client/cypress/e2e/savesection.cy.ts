2270551c74e7614040c9cb5b229a901ec19af9f1
steven miller <sjmiller0403@gmail.com>
2023-04-17 14:22:04 -0400

    savesection test partially works

--- client/cypress/e2e/savesection.cy.ts
+++ client/cypress/e2e/savesection.cy.ts
@@ -1,6 +1,6 @@
 describe('save a section', () =>
 {
-  it('like thread', ()=> {
+  it('save a section and unsave', ()=> {
     cy.visit('')
     cy.get('.absolute > [href="/sign-in"] > #sign-in').click();
     cy.get("#email").type("millersteven@ufl.edu");
@@ -8,6 +8,20 @@ describe('save a section', () =>
     cy.get("#submit").click();
     cy.wait(1000);
     cy.get('.mt-2 > :nth-child(1) > .text-lg').click();
+    cy.get("#bookmark-section").click();
+    cy.get("#bookmark-section").should("be.true");
+    cy.get('#logo').click();
+  })
+  
+  it('save a subsection', ()=> {
+    cy.visit('')
+    cy.get('.absolute > [href="/sign-in"] > #sign-in').click();
+    cy.get("#email").type("millersteven@ufl.edu");
+    cy.get("#password").type("KfkGt2J2sAwA9tg");
+    cy.get("#submit").click();
+    cy.wait(2000);
+    cy.get('.mt-2 > :nth-child(2)').click();
+    cy.get('.sm\:grid > :nth-child(1)').click();
     cy.get("#bookmark-section").click();
     cy.get('#logo').click();
   })

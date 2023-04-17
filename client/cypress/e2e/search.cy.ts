1ce760bb0c31e86459c8272de6b217d42b34ab6c
steven miller <sjmiller0403@gmail.com>
2023-04-17 14:39:36 -0400

    search test works

--- client/cypress/e2e/search.cy.ts
+++ client/cypress/e2e/search.cy.ts
@@ -1,5 +1,6 @@
 describe('template spec', () => 
 {
+
   it('search for a section', () => 
   {
     cy.visit('')
@@ -25,6 +26,25 @@ describe('template spec', () =>
    // cy.wait(1000);
     //cy.get('[data-test="search-input"]').type("r")
     cy.get('.ellipsis').should("contain.text","No results");
+    cy.wait(2000);
+  })
+  
+  it('search within a section', () => 
+  {
+    cy.visit('')
+    cy.get('[data-test="search-input"]').type("S")
+    cy.wait(1000);
+    cy.get('[data-test="search-input"]').type("ports")
+    //cy.wait(1000);
+    //y.get('[data-test="search-input"]').type("o")
+   // cy.wait(1000);
+    //cy.get('[data-test="search-input"]').type("r")
+    cy.get(':nth-child(1) > .ellipsis > .block').should("contain.text","Sports")
+    cy.get(':nth-child(1) > .ellipsis > .block').click();
+    cy.wait(2000);
+    cy.get('[data-test="search-input"]').type("football");
+    cy.get(':nth-child(1) > .ellipsis > .block').should("contain.text","Football");
+    cy.get(':nth-child(1) > .ellipsis > .block').click();
     cy.wait(2000);
   })
 })

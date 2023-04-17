05bbd96468359211547d2b2d0b2522b89061c2ba
steven miller <sjmiller0403@gmail.com>
2023-04-16 20:48:44 -0400

    added new tests to forgot password

--- client/cypress/e2e/ForgotPassword.cy.ts
+++ client/cypress/e2e/ForgotPassword.cy.ts
@@ -1,10 +1,29 @@
-describe('forgot password spec', () => 
+describe('resetting password with incorrect code', () => 
 {
-  //need to find a way to verify we actually got the email
+  
   it('reset password', ()=> {
     cy.visit('/sign-in')
     cy.get("#forgot-password").click()
     cy.get("#email").type("millersteven@ufl.edu")
-    cy.get("#send").click();
+    //cy.get("#send-email").click();
+    //cy.wait(10000);
+    cy.get("#code").type("000000");
+    cy.get("#password").type("Miller0403");
+    cy.get("#check").click();
   })
 })
+describe('resetting password without password', () => 
+{
+  
+  it('reset password', ()=> {
+    cy.visit('/sign-in')
+    cy.get("#forgot-password").click()
+    cy.get("#email").type("millersteven@ufl.edu")
+    //cy.get("#send-email").click();
+    //cy.wait(10000);
+    cy.get("#code").type("000000");
+    //cy.get("#password").type("Miller0403");
+    cy.get("#check").click();
+  })
+})
+

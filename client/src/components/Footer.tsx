fcd1a8a289fc372b4d557929c92613a9bdc0d316
steven miller <sjmiller0403@gmail.com>
2023-04-17 15:05:08 -0400

    removed emailing list from home page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -121,44 +121,9 @@ const Footer = () => {
               )}
             </div>
 
-            {/* Newsletter */}
-            <div className="col-span-2 mt-4 lg:mt-0">
-              <div>
-                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                  Get the latest news!
-                </h2>
-
-                <p className="mt-4 text-gray-500 dark:text-gray-400">
-                  Subscribe to our newsletter to receive updates about our
-                  website.
-                </p>
-              </div>
-            </div>
-
-            <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end">
-              <form className="w-full">
-                <label htmlFor="UserEmail" className="sr-only">
-                  Email
-                </label>
-
-                <div className="border border-gray-100 p-2 focus-within:ring dark:border-gray-800 sm:flex sm:items-center sm:gap-4">
-                  <input
-                    type="email"
-                    id="UserEmail"
-                    placeholder="email@email.com"
-                    className="w-full border-none !outline-none focus:border-transparent focus:ring-transparent dark:bg-gray-900 dark:text-white sm:text-sm
-                    "
-                  />
-
-                  <button className="mt-1 w-full rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium tracking-wide text-white transition-none hover:bg-blue-600 sm:mt-0 sm:w-auto sm:flex-shrink-0">
-                    Sign Up
-                  </button>
-                </div>
-              </form>
-            </div>
           </div>
         </div>
-
+        
         <div className="mt-8 border-t border-gray-100 pt-8 dark:border-gray-800">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
             <p className="text-left text-xs text-gray-500 dark:text-gray-400">
